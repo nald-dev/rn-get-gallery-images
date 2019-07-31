cfe1abea8e993dcd3281513361481435af8e84f1
reynova <renalpraba@gmail.com>
2019-07-31 10:00:30 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+import {
+    CameraRoll,
+    Dimensions,
+    FlatList,
+    Image,
+    PermissionsAndroid,
+    TouchableOpacity,
+    View,
+} from 'react-native'
+
+export default class App extends React.Component {
+    state = {
+        imageUris: [],
+    }
+    
+    componentDidMount() {
+        this.GetPhotos()
+    }
+
+    async GetPhotos() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                {
+                'title': 'Cool Photo App Camera Permission',
+                'message': 'Cool Photo App needs access to your camera so you can take awesome pictures.'
+                }
+            )
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                CameraRoll.getPhotos({
+                    first: 20,
+                    assetType: 'All'
+                })
+                .then(r => {
+                    let imageUris = []
+
+                    for(let imageUrisIndex = 0; imageUrisIndex < r.edges.length; imageUrisIndex++) {
+                        imageUris.push(r.edges[imageUrisIndex].node.image.uri)
+                    }
+        
+                    this.setState({ imageUris: imageUris })
+                })
+            } else {
+                console.log("Camera permission denied")
+            }
+            } catch (err) {
+            console.warn(err)
+        }
+    }
+
+    render() {
+        return (
+            <View
+                style = {{
+                    flex: 1,
+                }}
+            >
+                <FlatList
+                    data = {this.state.imageUris}
+                    numColumns = {2}
+                    keyExtractor = {(item, index) => index.toString()}
+                    renderItem={({item}) => {
+                        return (
+                            <TouchableOpacity
+                                style = {{
+                                    backgroundColor: "black",
+                                    padding: 2,
+                                }}
+                            >
+                                <Image
+                                    resizeMode = "cover"
+                                    source = {{uri: item}}
+                                    style = {{
+                                        height: Dimensions.get("screen").width / 2,
+                                        width: Dimensions.get("screen").width / 2,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+            </View>
+        )
+    }
+}
